e6694f7541f15f5e27d5946c5bfa158cfceadccc
Lovelyfin00 <omokaroloveth10@gmail.com>
2023-01-10 19:09:34 +0100

    changed the method of post request from async to form submit and finalized code for the action controller

--- client/src/controllers/index.ts
+++ client/src/controllers/index.ts
@@ -1,12 +1,12 @@
 import type { Definition } from '@hotwired/stimulus';
 
-import { AutoFieldController } from './AutoFieldController';
 import { ActionController } from './ActionController';
+import { AutoFieldController } from './AutoFieldController';
 
 /**
  * Important: Only add default core controllers that should load with the base admin JS bundle.
  */
 export const coreControllerDefinitions: Definition[] = [
-  { controllerConstructor: AutoFieldController, identifier: 'w-auto-field' },
   { controllerConstructor: ActionController, identifier: 'w-action' },
+  { controllerConstructor: AutoFieldController, identifier: 'w-auto-field' },
 ];
